fcd8f55e2aacaee195fffff1213fa961eedfa08e
Luke Wendling <luke@lukewendling.com>
2015-11-13 16:09:24 -0600

    move heatmap data retrieval, settings to service

--- client/app/modules/events-map/directives/map.js
+++ client/app/modules/events-map/directives/map.js
@@ -1,201 +1,16 @@
 'use strict';
 angular.module('gennie.eventsMap')
-  .directive('map', function ($rootScope) {
-    var heatmap;
-    
+  .directive('map', ['mapService', function (mapService) {
     function link(scope, element, attrs) {
       var mapOptions = {
         zoom: 13,
         center: {lat: 37.775, lng: -122.434},
-        styles: [
-          {
-              "featureType": "all",
-              "elementType": "labels.text.fill",
-              "stylers": [
-                  {
-                      "saturation": 36
-                  },
-                  {
-                      "color": "#181818"
-                  },
-                  {
-                      "lightness": 40
-                  }
-              ]
-          },
-          {
-              "featureType": "all",
-              "elementType": "labels.text.stroke",
-              "stylers": [
-                  {
-                      "visibility": "on"
-                  },
-                  {
-                      "color": "#181818"
-                  },
-                  {
-                      "lightness": 16
-                  }
-              ]
-          },
-          {
-              "featureType": "all",
-              "elementType": "labels.icon",
-              "stylers": [
-                  {
-                      "visibility": "off"
-                  }
-              ]
-          },
-          {
-              "featureType": "administrative",
-              "elementType": "geometry.fill",
-              "stylers": [
-                  {
-                      "color": "#181818"
-                  },
-                  {
-                      "lightness": 20
-                  }
-              ]
-          },
-          {
-              "featureType": "administrative",
-              "elementType": "geometry.stroke",
-              "stylers": [
-                  {
-                      "color": "#181818"
-                  },
-                  {
-                      "lightness": 17
-                  },
-                  {
-                      "weight": 1.2
-                  }
-              ]
-          },
-          {
-              "featureType": "landscape",
-              "elementType": "geometry",
-              "stylers": [
-                  {
-                      "color": "#181818"
-                  },
-                  {
-                      "lightness": 20
-                  }
-              ]
-          },
-          {
-              "featureType": "poi",
-              "elementType": "geometry",
-              "stylers": [
-                  {
-                      "color": "#181818"
-                  },
-                  {
-                      "lightness": 21
-                  }
-              ]
-          },
-          {
-              "featureType": "road.highway",
-              "elementType": "geometry.fill",
-              "stylers": [
-                  {
-                      "color": "#181818"
-                  },
-                  {
-                      "lightness": 17
-                  }
-              ]
-          },
-          {
-              "featureType": "road.highway",
-              "elementType": "geometry.stroke",
-              "stylers": [
-                  {
-                      "color": "#181818"
-                  },
-                  {
-                      "lightness": 29
-                  },
-                  {
-                      "weight": 0.2
-                  }
-              ]
-          },
-          {
-              "featureType": "road.arterial",
-              "elementType": "geometry",
-              "stylers": [
-                  {
-                      "color": "#181818"
-                  },
-                  {
-                      "lightness": 18
-                  }
-              ]
-          },
-          {
-              "featureType": "road.local",
-              "elementType": "geometry",
-              "stylers": [
-                  {
-                      "color": "#181818"
-                  },
-                  {
-                      "lightness": 16
-                  }
-              ]
-          },
-          {
-              "featureType": "transit",
-              "elementType": "geometry",
-              "stylers": [
-                  {
-                      "color": "#181818"
-                  },
-                  {
-                      "lightness": 19
-                  }
-              ]
-          },
-          {
-              "featureType": "water",
-              "elementType": "geometry",
-              "stylers": [
-                  {
-                      "color": "#181818"
-                  },
-                  {
-                      
-                  }
-              ]
-          }
-      ]
+        styles: mapService.darkStyles
       }
 
-      scope.map = new google.maps.Map(element[0], mapOptions);
+      scope.map = new google.maps.Map(element[0], mapOptions)
 
-      var Event = Parse.Object.extend("Event")
-      var query = new Parse.Query(Event)
-      
-      query.find({
-        success: function(results) {
-          heatmap = new google.maps.visualization.HeatmapLayer({
-            data: _.map(results, 
-              function(item) {
-                var coord = item.get('geo')
-                return new google.maps.LatLng(coord[0], coord[1]) 
-              }),
-            map: scope.map
-          });
-        },
-        error: function(err) {
-          console.log(err)
-        }
-      })
+      mapService.applyHeatmap(scope.map)
 
     }
 
@@ -203,4 +18,4 @@ angular.module('gennie.eventsMap')
       restrict: 'AE',
       link: link
     };
-  });
+  }]);
--- client/app/modules/events-map/services/map.js
+++ client/app/modules/events-map/services/map.js
@@ -1,8 +1,195 @@
 'use strict';
 angular.module('gennie.eventsMap')
-  .factory('MapService', function($cookies, $http) {
-    var self = {
+  .factory('mapService', function() {
+    var darkStyles = [
+    {
+      "featureType": "all",
+      "elementType": "labels.text.fill",
+      "stylers": [
+        {
+          "saturation": 36
+        },
+        {
+          "color": "#181818"
+        },
+        {
+          "lightness": 40
+        }
+      ]
+    },
+    {
+      "featureType": "all",
+      "elementType": "labels.text.stroke",
+      "stylers": [
+        {
+          "visibility": "on"
+        },
+        {
+          "color": "#181818"
+        },
+        {
+          "lightness": 16
+        }
+      ]
+    },
+    {
+      "featureType": "all",
+      "elementType": "labels.icon",
+      "stylers": [
+        {
+          "visibility": "off"
+        }
+      ]
+    },
+    {
+      "featureType": "administrative",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#181818"
+        },
+        {
+          "lightness": 20
+        }
+      ]
+    },
+    {
+      "featureType": "administrative",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+          "color": "#181818"
+        },
+        {
+          "lightness": 17
+        },
+        {
+          "weight": 1.2
+        }
+      ]
+    },
+    {
+      "featureType": "landscape",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#181818"
+        },
+        {
+          "lightness": 20
+        }
+      ]
+    },
+    {
+      "featureType": "poi",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#181818"
+        },
+        {
+          "lightness": 21
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry.fill",
+      "stylers": [
+        {
+          "color": "#181818"
+        },
+        {
+          "lightness": 17
+        }
+      ]
+    },
+    {
+      "featureType": "road.highway",
+      "elementType": "geometry.stroke",
+      "stylers": [
+        {
+          "color": "#181818"
+        },
+        {
+          "lightness": 29
+        },
+        {
+          "weight": 0.2
+        }
+      ]
+    },
+    {
+      "featureType": "road.arterial",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#181818"
+        },
+        {
+          "lightness": 18
+        }
+      ]
+    },
+    {
+      "featureType": "road.local",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#181818"
+        },
+        {
+          "lightness": 16
+        }
+      ]
+    },
+    {
+      "featureType": "transit",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#181818"
+        },
+        {
+          "lightness": 19
+        }
+      ]
+    },
+    {
+      "featureType": "water",
+      "elementType": "geometry",
+      "stylers": [
+        {
+          "color": "#181818"
+        },
+        {
+          
+        }
+      ]
+    }
+    ];
 
+    return {
+      applyHeatmap: function(map) {
+        var Event = Parse.Object.extend("Event")
+        var query = new Parse.Query(Event)
+        
+        query.find({
+          success: function(results) {
+            new google.maps.visualization.HeatmapLayer({
+              data: _.map(results, 
+                function(item) {
+                  var coord = item.get('geo')
+                  return new google.maps.LatLng(coord[0], coord[1]) 
+                }),
+              map: map
+            });
+          },
+          error: function(err) {
+            console.log(err)
+          }
+        })
+      },
+      darkStyles: darkStyles
     };
-    return self;
   });
