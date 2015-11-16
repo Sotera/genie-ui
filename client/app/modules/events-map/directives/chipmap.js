62e138761e79e3b47a0555f157448d5ea859fb57
Luke Wendling <luke@lukewendling.com>
2015-11-16 10:29:09 -0600

    touch up chipmap directive. remove signed-in param from gmap

--- client/app/modules/events-map/directives/chipmap.js
+++ client/app/modules/events-map/directives/chipmap.js
@@ -1,21 +1,22 @@
 'use strict';
 angular.module('genie.eventsMap')
   .directive('chipmap', ['mapService', function (mapService) {
-    function link(scope, element, attrs) {
+    function link(scope, elem, attrs) {
 
       var mapOptions = {
         zoom: 1,
         center: {lat: 37.775, lng: -122.434},
         styles: mapService.darkStyles
       };
-      scope.map = new google.maps.Map(document.getElementById('chipmap_div'), mapOptions)
+      scope.map = new google.maps.Map(elem[0], mapOptions)
       mapService.applyHeatmap(scope.map)
 
     }
 
     return {
       restrict: 'E',
+      replace: true,
       link: link,
-      template: '<div id="chipmap_div" style="width: 100%; height: 100%;">'
+      template: '<div style="width: 100%; height: 100%;">'
     };
   }]);
