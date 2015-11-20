87d4a266744b8656ce6325cae8014a7755c2fde8
Justin Lueders <justin.lueders@gmail.com>
2015-11-20 10:30:01 -0600

    merge PR #4: add genie icons. clean up blank module

--- client/app/modules/blank/config/blank.config.js
+++ client/app/modules/blank/config/blank.config.js
@@ -2,7 +2,7 @@
 angular.module('com.module.blank')
   .run(function ($rootScope, gettextCatalog) {
     $rootScope.addMenu(gettextCatalog.getString(' Blank'), 'app.blank.list','fa-bug');
-    $rootScope.addDashboardBox(gettextCatalog.getString(' Blank'), 'bg-blue5', 'ion-bug',
+    $rootScope.addDashboardBox(gettextCatalog.getString(' Blank'), 'bg-blue5', '',
         5, 'app.blank.list','modules/blank/views/chip');
 
   });
--- client/app/modules/blank/directives/blank.chart.directive.js
+++ client/app/modules/blank/directives/blank.chart.directive.js
@@ -6,33 +6,50 @@ angular.module('com.module.blank')
       template: '<div id="chart_div" style="width: 100%; height: 100%;">',
       restrict: 'E',
       link:function(scope, element, attrs) {
-        // Set a callback to run when the Google Visualization API is loaded.
-        //google.setOnLoadCallback(function() {
-
-          // Create the data table.
           var data = new google.visualization.DataTable();
-          data.addColumn('string', 'Topping');
-          data.addColumn('number', 'Slices');
+          data.addColumn('number', 'Day');
+          data.addColumn('number', 'blank value');
+          data.addColumn('number', 'blank value 2');
+          data.addColumn('number', 'blank value 3');
+
           data.addRows([
-            ['Mushrooms', 3],
-            ['Onions', 1],
-            ['Olives', 1],
-            ['Zucchini', 1],
-            ['Pepperoni', 2]
+              [1,  37.8, 80.8, 41.8],
+              [2,  30.9, 69.5, 32.4],
+              [3,  25.4,   57, 25.7],
+              [4,  11.7, 18.8, 10.5],
+              [5,  11.9, 17.6, 10.4],
+              [6,   8.8, 13.6,  7.7],
+              [7,   7.6, 12.3,  9.6],
+              [8,  12.3, 29.2, 10.6],
+              [9,  16.9, 42.9, 14.8],
+              [10, 12.8, 30.9, 11.6],
+              [11,  5.3,  7.9,  4.7],
+              [12,  6.6,  8.4,  5.2],
+              [13,  4.8,  6.3,  3.6],
+              [14,  4.2,  6.2,  3.4]
           ]);
 
-          // Set chart options
           var options = {
-            
-            'backgroundColor':'#181818',
-            'pieSliceText':'none',
-            'is3D': true,
-            'legend':'none'
+              chart: {
+                  'backgroundColor': {
+                      'fill': '#181818'
+                  }
+
+              },
+              'chartArea': {
+                  'backgroundColor': '#181818'
+              },
+              axes: {
+                  x: {
+                      0: {side: 'top'}
+                  }
+              }
           };
 
-          // Instantiate and draw our chart, passing in some options.
-          var chart = new google.visualization.PieChart(document.getElementById('chart_div'));
-          chart.draw(data, options);
+          var chart = new google.charts.Line(document.getElementById('chart_div'));
+
+          chart.draw(data, google.charts.Line.convertOptions(options));
+
         //});
       }
     };
