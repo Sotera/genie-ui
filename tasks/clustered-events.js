a74ac359af414474302ad758a06fe58654dc57c5
Luke Wendling <luke@lukewendling.com>
2015-11-25 13:17:08 -0600

    add Task transform attr

--- tasks/clustered-events.js
+++ tasks/clustered-events.js
@@ -12,7 +12,8 @@ Task.findOrCreate({
   },
   {
     code: taskName,
-    lastRun: '2015-11-01'
+    lastRun: '2015-11-01',
+    transform: 'basic'
   },
   runTask);
 
@@ -29,15 +30,17 @@ function runTask (err, task) {
       indexedDate: {gt: task.lastRun}
     },
     limit: 50
-  }, processEventSources)
+  }, processEventSources(task))
 }
 
-function processEventSources (err, eventSources) {
-  if (err) {
-    log(err);
-    return;
-  }
+function processEventSources (task) {
+  return function(err, eventSources) {
+    if (err) {
+      log(err);
+      return;
+    }
 
-  worker.run({eventSources: eventSources, transform: 'basic'});
-  // TODO: update task last run
+    worker.run({eventSources: eventSources, transform: task.transform});
+    // TODO: update task last run
+  }
 }
