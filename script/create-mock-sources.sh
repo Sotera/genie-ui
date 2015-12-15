ceb351d6558206d84391d19f77eccededba3f579
Luke Wendling <luke@lukewendling.com>
2015-12-15 11:56:54 -0600

    rm dupe mocks

--- script/create-mock-sources.sh
+++ script/create-mock-sources.sh
@@ -3,8 +3,6 @@
 # delete index and bulk import from file
 curl -X DELETE localhost:9200/genie
 
-curl -X POST localhost:9200/genie/post/_bulk --data-binary @script/mock-sources.json
+curl -X POST localhost:9200/genie/post/_bulk --data-binary @import/mock-sources.json
 
 curl localhost:9200/genie/post/_search
-
-# curl -X DELETE localhost:9200/genie/post/<id>
