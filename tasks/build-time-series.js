60950fce0cac8353a95a307237fc4c47bde22d29
Luke Wendling <luke@lukewendling.com>
2015-12-16 15:00:42 -0600

    use clustering start,end dates to transform and build time series chart

---
+++ tasks/build-time-series.js
@@ -0,0 +1,60 @@
+#!/usr/bin/env node
+'use strict';
+
+var app = require('../server/server'),
+  Chart = app.models.Chart,
+  ZoomLevel = app.models.ZoomLevel,
+  log = require('debug')('task:build-time-series'),
+  moment = require('moment'),
+  settings = require('../server/util/get-settings'),
+  time = require('../server/util/time');
+
+
+settings(['map:maxZoom', 'zoomLevels:endDate'], findZoomLevel);
+
+function findZoomLevel(settings) {
+  ZoomLevel.find(
+    { where: {zoomLevel: settings.maxZoom} },
+    createChart(settings)
+  );
+}
+
+function createChart(settings) {
+  return (err, zoomLevels) => {
+    if (err) {
+      log(err);
+      return;
+    }
+
+    let endDate = moment(settings['zoomLevels:endDate'] || moment());
+
+    let rows = zoomLevels.map(zoomLevel => {
+      let date = endDate.subtract(zoomLevel.minutesAgo, 'minutes');
+      return [date.format('YYYY-MM-DD'), zoomLevel.events.length];
+    });
+
+    let chartData = {
+      rows: rows,
+      columns: [
+        {"type": "date", "label": "Date"},
+        {"type": "number", "label": "Events"}
+      ]
+    };
+
+    Chart.findOrCreate(
+      { where: {name: 'time-series'} },
+      {
+        name: 'time-series',
+        data: chartData
+      },
+      (err, chart) => {
+        if (err) {
+          log(err);
+          return;
+        }
+        chart.data = chartData;
+        chart.save();
+      }
+    );
+  }
+}
