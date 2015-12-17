d2327d4eb6fd3cfda7ba0f461a0b9b59ad3aa70c
John Reeme <john.reeme@soteradefense.com>
2015-12-17 10:08:43 -0600

    Need to switch back to <master> for a minute

--- server/util/twitter-client.js
+++ server/util/twitter-client.js
@@ -4,16 +4,33 @@ var Twitter = require('twitter');
 
 module.exports = class {
   constructor() {
-    var twitterClientOptions = {
-      consumer_key: process.env.CONSUMER_KEY,
-      consumer_secret: process.env.CONSUMER_SECRET,
-      access_token_key: process.env.ACCESS_TOKEN,
-      access_token_secret: process.env.ACCESS_TOKEN_SECRET
-    };
-    this.client = new Twitter(twitterClientOptions);
+    try{
+      var twitterKeyFilename = require('path').join(__dirname, '../../.twitter-keys.json');
+      this.twitterKeys = JSON.parse(require('fs').readFileSync(twitterKeyFilename, 'utf8'));
+      this.twitterClients = twitterKeys.map(function(twitterKey){
+        return new Twitter(twitterKey);
+      });
+    }catch(err){
+      log(err);
+    }
   }
+
   accountSettings(cb) {
-    this.client.get('account/settings', cb);
+    //this.client.get('account/settings', cb);
+    log('>>>>> Streaming->');
+    var options = {
+      stall_warnings: true,
+      locations: '-91.71,37.58,-80.54,42.92'
+    };
+    this.client.stream('statuses/filter', options, function (stream) {
+      stream.on('data', function (tweet) {
+        log(tweet);
+      });
+
+      stream.on('error', function (err) {
+        log(err);
+      });
+    });
   }
 }
 
