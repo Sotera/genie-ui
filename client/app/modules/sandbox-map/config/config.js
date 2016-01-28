4959e89da5960323a7f478bed35275ff226631d9
Luke Wendling <luke@lukewendling.com>
2016-01-28 15:37:33 -0600

    rm sandbox temp module

--- client/app/modules/sandbox-map/config/config.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-angular.module('genie.sandboxMap')
-.run(function ($rootScope) {
-  $rootScope.addMenu('Sandbox Trial', 'app.sandbox-map.show','fa-map');
-  $rootScope.addDashboardBox('Sandbox Trial', 'bg-blue5', '',
-    5, 'app.sandbox-map.show','modules/sandbox-map/views/dash.html');
-
-});
