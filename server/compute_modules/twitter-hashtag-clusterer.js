7623d733ad508ddfaacb9e5c1850d9cd186ac4e1
jreeme <john.reeme@gmail.com>
2016-02-04 15:18:15 -0600

    stash

--- server/compute_modules/twitter-hashtag-clusterer.js
+++ server/compute_modules/twitter-hashtag-clusterer.js
@@ -7,7 +7,6 @@ var moment = require('moment');
 var loopback = require('loopback');
 var clustering = require('density-clustering');
 var LoopbackModelHelper = require('../util/loopback-model-helper');
-var ScoreRecord = require('../compute_modules/geo-tweet-score-record');
 var Random = require('random-js');
 var ensureStringArray = require('../util/ensure-string-array');
 
@@ -28,220 +27,7 @@ module.exports = class {
     this.scoredGeoTweetHelper = new LoopbackModelHelper('ScoredGeoTweet');
     this.geoTweetHashtagIndexHelper = new LoopbackModelHelper('GeoTweetHashtagIndex');
     this.hashtagEventsSourceHelper = new LoopbackModelHelper('HashtagEventsSource');
-  }
-
-  post_deprecated_I_think_clusterScoredRecords(options, cb) {
-    options = options || {};
-    async.waterfall(
-      [
-        function getScoredRecordCount(cb) {
-          scoredGeoTweetHelper.count(function (err, count) {
-            cb(err, count);
-          });
-        },
-        function getClusterTimeWindow(count, cb) {
-          try {
-            options.count = count;
-            options.blackList = options.blackList || [];
-            options.dbscanEpsilonMeters = options.dbscanEpsilonMeters || 500;
-            options.dbscanMinMembersInCluster = options.dbscanMinMembersInCluster || 5;
-            //See if caller provided us an end date
-            /*              options.startDate = '2016-01-04T20:06:00.000Z';
-             options.endDate = '2016-01-05T20:06:00.000Z';
-             options.clusteringWindowMinutes = 4 * 60;*/
-            options.minTweetsToCluster = options.minTweetsToCluster || 5;
-            options.minTweetsToCluster = (options.minTweetsToCluster < 3)
-              ? 3
-              : (options.minTweetsToCluster > 10)
-              ? 10
-              : options.minTweetsToCluster;
-            options._filterClusteringWindowMinutes = options.clusteringWindowMinutes || 8 * 60;
-            options._filterClusteringWindowMinutes = (options._filterClusteringWindowMinutes < 60)
-              ? 60
-              : (options._filterClusteringWindowMinutes > (8 * 60))
-              ? (8 * 60)
-              : options._filterClusteringWindowMinutes;
-
-            options._filterStartDate = (isNaN(Date.parse(options.startDate)))
-              ? null
-              : moment(Date.parse(options.startDate));
-
-            options._filterEndDate = (isNaN(Date.parse(options.endDate)))
-              ? null
-              : moment(Date.parse(options.endDate));
-          } catch (err) {
-            options._filterStartDate = options._filterEndDate = null;
-          }
-          //If we can't get enough info from user about clustering time window then get latest
-          //date from data and go back _filterClusteringWindowMinutes to create window
-          if (options._filterStartDate && options._filterEndDate) {
-            cb(null, options);
-          } else if (!options._filterStartDate && !options._filterEndDate) {
-            scoredGeoTweetHelper.find(
-              function (err, timeWindowedScoredGeoTweets) {
-                var minDate = new Date();
-                var maxDate = new Date('1900-01-01');
-                timeWindowedScoredGeoTweets.forEach(function (tweet) {
-                  var postDate = new Date(tweet.postDate);
-                  minDate = (postDate < minDate) ? postDate : minDate;
-                  maxDate = (postDate > maxDate) ? postDate : maxDate;
-                });
-                options._filterEndDate = moment(maxDate);
-                options._filterStartDate = moment(maxDate);
-                options._filterStartDate.subtract(options._filterClusteringWindowMinutes, 'minutes');
-                cb(null, options);
-              }
-            );
-          } else if (options._filterStartDate) {
-            options._filterEndDate = moment(options._filterStartDate);
-            options._filterEndDate.add(options._filterClusteringWindowMinutes, 'minutes');
-            cb(null, options);
-          } else if (options._filterEndDate) {
-            options._filterStartDate = moment(options._filterEndDate);
-            options._filterEndDate.subtract(options._filterClusteringWindowMinutes, 'minutes');
-            cb(null, options);
-          }
-        },
-        function (options, cb) {
-          scoredGeoTweetHelper.find({
-              where: {
-                and: [
-                  {postDate: {gte: options._filterStartDate}},
-                  {postDate: {lte: options._filterEndDate}}
-                ]
-              },
-              limit: options.count
-            },
-            function (err, timeWindowedScoredGeoTweets) {
-              cb(null, options, timeWindowedScoredGeoTweets);
-            });
-        },
-        function (options, timeWindowedScoredGeoTweets, cb) {
-          var geoTweetBuckets = {};
-          timeWindowedScoredGeoTweets.forEach(function (sr) {
-            var tagArray = sr.tags.split(',');
-            tagArray.forEach(function (tagText) {
-              if (options.blackList.indexOf(tagText) != -1) {
-                return;
-              }
-              if (geoTweetBuckets[tagText]) {
-                geoTweetBuckets[tagText].push(sr);
-              } else {
-                geoTweetBuckets[tagText] = [sr];
-              }
-            });
-          });
-          cb(null, options, geoTweetBuckets);
-        },
-        function (options, geoTweetBuckets, cb) {
-          var loc0 = new loopback.GeoPoint({lat: 0, lng: 0});
-          var loc1 = new loopback.GeoPoint({lat: 0, lng: 0});
-          for (var geoTweetBucket in geoTweetBuckets) {
-            var geoTweetArray = geoTweetBuckets[geoTweetBucket];
-            var dataToCluster = geoTweetArray.map(function (geoTweet) {
-              return [geoTweet.lng, geoTweet.lat];
-            });
-            geoTweetArray.clusters = dbscan.run(
-              dataToCluster,
-              options.dbscanEpsilonMeters,
-              options.dbscanMinMembersInCluster,
-              function (p, q) {
-                if (p.length != 2 || q.length != 2) {
-                  return Number.MAX_VALUE;
-                }
-                //Quick look to exclude very far away
-                /*                p = [-97,30.99];
-                 q = [-96.99,31];*/
-                var quickDist = (((p[1] - q[1]) * (p[1] - q[1])) + ((p[0] - q[0]) * (p[0] - q[0])));
-                if (quickDist > 0.001) {
-                  return Number.MAX_VALUE;
-                }
-                loc0.lat = p[1];
-                loc0.lng = p[0];
-                loc1.lat = q[1];
-                loc1.lng = q[0];
-                var distanceMeters = loopback.GeoPoint.distanceBetween(loc0, loc1, {type: 'meters'});
-                return distanceMeters;
-              });
-          }
-          cb(null, options, geoTweetBuckets);
-        },
-        function (options, geoTweetBuckets, cb) {
-          for (var geoTweetBucket in geoTweetBuckets) {
-            var geoTweetArray = geoTweetBuckets[geoTweetBucket];
-            geoTweetArray = geoTweetBuckets[geoTweetBucket];
-          }
-          cb();
-        }
-      ],
-      function (err, results) {
-        var e = err;
-      });
-  }
-
-
-  post_deprecated_I_think_ProcessNewTweets(options, cb) {
-    options = options || {};
-    var self = this;
-    async.waterfall(
-      [
-        //Doing it all in memory for now. Later we may add ES query result paging, etc.
-        //(but probably not)
-        function getGeoTweetCount(cb) {
-          //Get number of GeoTweets so we can set an ES query limit. If we don't set one we'll
-          //get 10 documents returned. Note that {where: {scored: false}} is ignored by the ES
-          //Loopback data connector. 'count' will always return the total number of documents.
-
-          //self.geoTweetHelper.count({where: {scored: false}}, function (err, count) {
-          self.geoTweetHelper.count(function (err, count) {
-            cb(err, count);
-          });
-        },
-        function getGeoTweets(count, cb) {
-          //Retrieve all the unscored GeoTweets in one query (no paging)
-          //self.geoTweetHelper.find({where: {scored: false}, limit: count}, function (err, geoTweets) {
-          self.geoTweetHelper.find({limit: count}, function (err, geoTweets) {
-            cb(err, geoTweets)
-          });
-        },
-        function convertGeoTweetsToScoreRecords(geoTweets, cb) {
-          //Map the fields from the full tweet object we need to do clustering and put them in a
-          //nice array
-          var scoreRecords = [];
-          geoTweets.forEach(function (geoTweet) {
-            try {
-              if (!geoTweet) {
-                throw new Error('<null> GeoTweet!');
-              }
-              var fullTweet = JSON.parse(geoTweet.full_tweet);
-              //Now shall we score the tweet
-              //Set GeoTweet instance to scored so we don't look at it again
-              /*                geoTweet.updateAttribute('scored', true, function (err, o) {
-               var e = err;
-               });*/
-              var scoreRecord = new ScoreRecord(full_tweet);
-              scoreRecords.push(scoreRecord);
-            } catch (err) {
-              log(err);
-            }
-          });
-          cb(null, scoreRecords);
-        },
-        function putScoreRecordsIntoDatabase(scoreRecords, cb) {
-          //Take the nice array of ScoredGeoTweets and slam them in to ES. Check twitterId so we don't
-          //have any repeats (pretty unlikely but pretty easy to check so, why not?)
-          var queries = scoreRecords.map(function (sr) {
-            return {where: {twitterId: sr.id}};
-          });
-          scoredGeoTweetHelper.findOrCreateMany(queries, scoreRecords, function (err, newScoredRecords) {
-            cb(null);
-          });
-        }
-      ],
-      function (err, results) {
-        var e = err;
-      }
-    );
+    this.restTaskQueue = async.queue(this._callViaReST.bind(this), 2);
   }
 
   post_clusterHashtags(options, cb) {
@@ -661,7 +447,7 @@ module.exports = class {
     }
   }
 
-  post_restTranslateFileToGeoTweet(options, cb) {
+  post_translateFileToGeoTweet(options, cb) {
     var path = options.path;
     var JSONStream = require('JSONStream');
     var es = require('event-stream');
@@ -670,6 +456,13 @@ module.exports = class {
     fs.createReadStream(path, 'utf8')
       .pipe(JSONStream.parse('*'))
       .pipe(es.map(function (tweet, cb) {
+        self.restTaskQueue.push({
+          restMethod: 'convertTweetToGeoTweet',
+          tweet,
+          onlyWithLocation: true,
+          onlyWithHashtags: true
+        });
+        return;
         self.post_convertTweetToGeoTweet({
           tweet,
           onlyWithLocation: true,
@@ -691,40 +484,23 @@ module.exports = class {
       });
   }
 
-  post_translateFileToGeoTweet(options, cb) {
-    var request = require('request');
-    var host = this.app.get('host');
-    var port = this.app.get('port');
-
-    request.post({
-      url: 'http://' + host + ':' + port + '/TwitterHashtagClusterer/restTranslateFileToGeoTweet',
-      json: true,
-      body: options
-    }, function (err, response, body) {
-      cb(err, body);
-    });
-  }
-
   post_loadTestTweetFiles(options, cb) {
     var foldersToSearch = ensureStringArray(options.foldersToSearch);
     var globExpression = options.globExpression || '*';
     var glob = require('glob-fs')({gitignore: true});
     var self = this;
-    var taskQueue = async.queue(self.post_translateFileToGeoTweet.bind(self), 2);
     foldersToSearch.forEach(function (folderToSearch) {
       //In the spirit of scalability let's stream the globbed filenames
       var fileCount = 0;
       glob.readdirStream(globExpression, {cwd: folderToSearch})
         .on('data', function (file) {
           log('Queuing: ' + file.name + '[' + (++fileCount) + ']');
-          process.nextTick(()=> {
-            taskQueue.push({path: file.path}, function (err) {
-              if (err) {
-                log(err);
-                return;
-              }
-              log('Processed: ' + file.name);
-            });
+          self.restTaskQueue.push({
+            restMethod: 'translateFileToGeoTweet',
+            path: file.path,
+            cb: function(err, results){
+              var r = results;
+            }
           });
         })
         .on('error', function (err) {
@@ -734,6 +510,25 @@ module.exports = class {
         .on('end', function () {
           cb(null, {fileCount});
         });
+    });
+  }
+
+  _callViaReST(options, taskCb) {
+    var request = require('request');
+    var host = this.app.get('host');
+    var port = this.app.get('port');
+    var apiName = options.apiName || 'TwitterHashtagClusterer';
+    var cb = options.cb || function () {
+      };
+    var restMethod = options.restMethod;
+
+    request.post({
+      url: 'http://' + host + ':' + port + '/' + apiName + '/' + restMethod,
+      json: true,
+      body: options
+    }, function (err, response, body) {
+      cb(err, body);
+      taskCb();
     });
   }
 
