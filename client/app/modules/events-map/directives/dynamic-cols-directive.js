0ded9281b510c6886ae87c15545846c1cd57ba62
Luke Wendling <luke@lukewendling.com>
2016-02-05 16:31:43 -0600

    add directive to control events list display

---
+++ client/app/modules/events-map/directives/dynamic-cols-directive.js
@@ -0,0 +1,27 @@
+'use strict';
+angular.module('genie.eventsMap')
+// watch scope variable (like 'events'), and swap class names on change.
+// expects watched var to be a string or array.
+.directive('dynamicCols', [function() {
+
+  function link(scope, elem, attrs) {
+    var before = JSON.parse(attrs.before);
+    var after = JSON.parse(attrs.after);
+
+    scope.colClasses = before;
+
+    scope.$watchCollection(attrs.dynamicCols,
+      function() {
+        if (scope[attrs.dynamicCols].length) {
+          scope.colClasses = after;
+        } else {
+          scope.colClasses = before;
+        }
+      }
+    );
+  }
+
+  return {
+    link: link
+  };
+}])
