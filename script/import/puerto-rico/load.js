a488ae443147455b7d71553bafa75c72963037c3
Luke Wendling <luke@lukewendling.com>
2016-03-21 14:01:00 -0500

    geocode puerto rico data before loading

--- script/import/puerto-rico/load.js
+++ script/import/puerto-rico/load.js
@@ -3,31 +3,79 @@
 
 var data = require('./OA_PR_JSON.json');
 var app = require('../../../server/server');
+var request = require('request-json');
+var client = request.createClient('http://localhost:3001/api/geocoder/');
 var _ = require('lodash');
 var GeoTweet = app.models.GeoTweet;
 
 var docs = data.docs;
 
-var createGeoTweets = docs.map(doc => {
-  var latlng = doc.latLon[0].split(',');
-  var lat = +latlng[0],
-    lng = +latlng[1];
+function requestPromise(loc, defaults /*i.e. {latitude: 0, longitude: 0}*/ ) {
+  return new Promise((resolve, reject) => {
+    client.post('forward-geo/', { address: loc }, (err, _, body) => {
+      // console.log(body)
+      if (err) {
+        console.log(err);
+        // return reject(err);
+      }
+      if (!body.length) {
+        return resolve([defaults]); // echo back defaults if no results
+      }
+      resolve(body);
+    });
+  });
+}
 
-  return GeoTweet.create({
+// geocode all locations in doc
+function geocode(doc) {
+  var locationNames = doc.locationName;
+  var latLngs = doc.latLon; // array of strings matching order of location names
+
+  var geocodePromises = locationNames.map((loc,i) => {
+    var latlng = latLngs[i].split(','),
+      lat = latlng[0],
+      lng = latlng[1];
+
+    // use the first result. TODO: use confidence score if available?
+    return requestPromise(loc, {latitude: lat, longitude: lng})
+    .then(results => {
+      doc.geo = results[0]; // add geo result to original doc
+      return doc;
+    });
+  });
+
+  return Promise.all(geocodePromises);
+}
+
+function createGeoTweet(doc) {
+  var geo = doc.geo;
+  GeoTweet.create({
     username: doc.feedName,
     tweet_id: doc.guid,
     full_tweet: doc.originalTitle,
     post_date: doc.datePublished,
     hashtags: _([doc.feedSource, doc.storySource, doc.all_KeyTag, doc.goWord, doc.locationName])
       .flatten().compact().uniq().value(),
-    lat: lat,
-    lng: lng
+    lat: geo.latitude,
+    lng: geo.longitude
   });
+}
+
+function tweetify(docs) {
+  // create geotweet record for each location (dupes tweet text)
+  docs.forEach(createGeoTweet);
+}
+
+// collect all geocode-to-geotweet promises
+var processDocs = docs.map(doc => {
+  return geocode(doc)
+  .then(tweetify);
 });
 
-Promise.all(createGeoTweets)
+Promise.all(processDocs)
 .then(() => console.log('done'))
 .catch(err => {
-  GeoTweet.destroyAll();
   console.error(err);
+  GeoTweet.destroyAll();
+  console.error('Removing geotweets records');
 });
