79ac30b34ef341532ed642dc213419e299f18157
John Reeme <john.reeme@soteradefense.com>
2016-05-13 10:57:43 -0500

    Added some code to look at GeoTweet locations and create a bounding box around the tweets in the event.

--- server/compute_modules/twitter-hashtag-clusterer.js
+++ server/compute_modules/twitter-hashtag-clusterer.js
@@ -173,16 +173,24 @@ module.exports = class {
             var hashtag = eventSourceCluster.hashtag;
             eventSourceCluster.clustersOfTweets.forEach(function (clusterOfTweets) {
               var numPosts = clusterOfTweets.length;
+              var s_lat = 100;
+              var n_lat = -100;
+              var w_lng = 400;
+              var e_lng = -400;
               var reduceResult = clusterOfTweets.reduce(function (prev, curr, idx) {
+                s_lat = (curr.lat < s_lat) ? curr.lat : s_lat;
+                n_lat = (curr.lat > n_lat) ? curr.lat : n_lat;
+                w_lng = (curr.lng < w_lng) ? curr.lng : w_lng;
+                e_lng = (curr.lng > e_lng) ? curr.lng : e_lng;
                 var source_data = prev.source_data;
                 source_data.push(curr.tweet_id);
                 return {
                   lat: prev.lat + curr.lat,
                   lng: prev.lng + curr.lng,
                   post_date: prev.post_date + curr.post_date.getTime(),
-                  source_data:source_data
+                  source_data: source_data
                 };
-              }, {lat: 0, lng: 0, post_date: 0,source_data:[]});
+              }, {lat: 0, lng: 0, post_date: 0, source_data: []});
               var lat = reduceResult.lat / numPosts;
               var lng = reduceResult.lng / numPosts;
               var post_date = reduceResult.post_date / numPosts;
@@ -192,10 +200,20 @@ module.exports = class {
                 event_id: random.uuid4().toString(),
                 unique_user_count: clusterOfTweets.uniqueUserCount,
                 num_posts: numPosts,
-                source_data:source_data,
+                source_data: source_data,
                 event_source: 'hashtag',
                 post_date: new Date(post_date),
                 indexed_date: new Date(),
+                bounding_box: {
+                  sw: {
+                    lat: s_lat,
+                    lng: w_lng,
+                  },
+                  ne: {
+                    lat: n_lat,
+                    lng: e_lng,
+                  }
+                },
                 hashtag,
                 lat,
                 lng
@@ -299,7 +317,7 @@ module.exports = class {
     var tweetsByHashtagArray = options.tweetsByHashtagArray;
     async.each(tweetIds,
       function (tweetId, cb) {
-        if(tweetsByHashtagArray.geo_tweet_ids.indexOf(tweetId) !== -1){
+        if (tweetsByHashtagArray.geo_tweet_ids.indexOf(tweetId) !== -1) {
           cb(null);
           return;
         }
@@ -592,13 +610,13 @@ module.exports = class {
               }
             });
             options.tweet = tweet;
-            self.post_convertTweetToGeoTweet(options, function(err, convertedTweet) {
+            self.post_convertTweetToGeoTweet(options, function (err, convertedTweet) {
               if (err) {
                 log(err);
                 return;
               }
               if (convertedTweet) {
-                self.geoTweetHelper.create(convertedTweet, function(err, geoTweet) {
+                self.geoTweetHelper.create(convertedTweet, function (err, geoTweet) {
                   if (err) {
                     log(err);
                     return;
